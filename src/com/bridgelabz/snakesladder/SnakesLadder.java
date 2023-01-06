7d783f8169f77c739555d85268ab01ba0a7573e9
shiwani <shiwanipaunikar1@gmail.com>
2023-01-06 14:32:25 -0800

    player starting  position is  added

--- src/com/bridgelabz/snakesladder/SnakesLadder.java
+++ src/com/bridgelabz/snakesladder/SnakesLadder.java
@@ -2,6 +2,10 @@ package com.bridgelabz.snakesladder;
 
 public class SnakesLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to snakes and ladder");
+        System.out.println("Welcome to snake simulator program ");
+        int playerPosition=0;
+
+        System.out.println("currunt position is:"+playerPosition);
+
     }
 }
