59bc96b13511e58d63fc7840e81e118356763ebb
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:42:31 -0800

    Added: player starting position is added

--- src/com/bridgelabz/snakesladder/SnakesLadder.java
+++ src/com/bridgelabz/snakesladder/SnakesLadder.java
@@ -3,7 +3,7 @@ package com.bridgelabz.snakesladder;
 public class SnakesLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake simulator program ");
-        int playerPosition=0;
+        int playerPosition = 0;
 
         System.out.println("currunt position is:"+playerPosition);
 
